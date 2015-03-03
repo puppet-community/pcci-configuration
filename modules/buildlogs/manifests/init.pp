4cb9210a3c94724cf5f7d5b5e5c5eb94fa3714e7
Spencer Krum <nibz@spencerkrum.com>
2015-03-02 19:44:37 -0800

    move loganalyze into buildlogs

--- modules/buildlogs/manifests/init.pp
+++ modules/buildlogs/manifests/init.pp
@@ -17,5 +17,55 @@ class buildlogs () {
     configure_firewall => false,
   }
 
+  httpd_mod { 'rewrite':
+    ensure => present,
+  }
+  httpd_mod { 'proxy':
+    ensure => present,
+  }
+  httpd_mod { 'proxy_http':
+    ensure => present,
+  }
+
+  $swift_authurl  = ""
+
+  package { 'keyring':
+    ensure   => 'latest',
+    provider => 'pip',
+  }
+
+  vcsrepo { '/opt/os-loganalyze':
+    ensure   => latest,
+    provider => git,
+    revision => 'master',
+    source   => 'https://git.openstack.org/openstack-infra/os-loganalyze',
+    require  => Package['keyring'],
+  }
+
+  exec { 'install_os-loganalyze':
+    command     => 'python setup.py install',
+    cwd         => '/opt/os-loganalyze',
+    path        => '/bin:/usr/bin',
+    refreshonly => true,
+    subscribe   => Vcsrepo['/opt/os-loganalyze'],
+  }
+
+  file { '/etc/os_loganalyze':
+    ensure  => directory,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    require => Vcsrepo['/opt/os-loganalyze'],
+  }
+
+  file { '/etc/os_loganalyze/wsgi.conf':
+    ensure  => present,
+    owner   => 'root',
+    group   => 'www-data',
+    mode    => '0440',
+    content => template('buildlogs/os-loganalyze-wsgi.conf.erb'),
+    require => File['/etc/os_loganalyze'],
+  }
+
 
 }
