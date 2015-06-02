204897e02d228a2ef7ab95eaf556410c976625d3
Igor Galić <i.galic@brainsware.org>
2015-06-02 22:52:35 +0200

    define a wrapper for creating ssh auth admin keys

---
+++ modules/admins/manifests/key.pp
@@ -0,0 +1,24 @@
+# == Define: admins::key
+# 
+# This is a wraper ssh_authorized_key to authorize both, the root and pcci user
+#
+define admins::key (
+    $key,
+    $ensure = 'present',
+    $type   = 'ssh-rsa',
+) {
+  
+  ssh_authorized_key { "${title}-pcci":
+    ensure => $ensure,
+    user   => 'pcci',
+    type   => $type,
+    key    => $key
+  }
+
+  ssh_authorized_key { "${title}-root":
+    ensure => $ensure,
+    user   => 'root',
+    type   => $type,
+    key    => $key
+  }
+}
